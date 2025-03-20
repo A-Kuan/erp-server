8cb0e0d35da78ed7a1764b796d8ad9a7479bd42c
Wenshi huang <wenshihuang@Wenshis-MacBook-Pro.local>
2025-03-20 21:06:42 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,7 @@ async fn main() -> Result<()> {
     let pool = create_pool(&database_url).await?;
     let current_dir = env::current_dir().unwrap();
     println!("Current working directory: {:?}", current_dir);
-    // 命令行测试部分
-    // let file_path = "static/test.xlsx";
-    //
-    // match calamine::read_excel(file_path) {
-    //     Ok(df) => {
-    //         println!("成功读取 DataFrame：\n{}", df);
-    //     },
-    //     Err(e) => {
-    //         eprintln!("读取 Excel 文件失败：{}", e);
-    //     },
-    // }
-    println!("启动服务器：访问 http://127.0.0.1:8080/read_excel?file_path=static/test.xlsx");
+    // println!("启动服务器：访问 http://127.0.0.1:8080/read_excel?file_path=static/test.xlsx");
 
     HttpServer::new(move || {
         App::new()
